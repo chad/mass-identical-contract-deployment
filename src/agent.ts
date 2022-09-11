375a6bb4bfec9bf182bfb9d11886c0dd030a3179
Chad Fowler <chad@chadfowler.com>
2022-09-11 14:28:03 -0500

    store records of contract deployments with timestamps

--- src/agent.ts
+++ src/agent.ts
@@ -7,6 +7,7 @@ import {
   FindingSeverity,
   FindingType,
 } from "forta-agent";
+import { sha256 } from "js-sha256";
 
 const sha = require("js-sha256");
 
@@ -18,6 +19,14 @@ export const TETHER_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 export const TETHER_DECIMALS = 6;
 let findingsCount = 0;
 
+/* FIXME: I believe this is safe because every agent is running in a single-threaded manner.  Need to validate
+*/
+
+type Deployment = {deployingContract: string, timestamp: number }
+
+const hashesOfContractsWeHaveSeen : {[codeHash: string] :  Array<Deployment>} = {}
+
+
 const handleTransaction: HandleTransaction = async (
   txEvent: TransactionEvent
 ) => {
@@ -26,22 +35,22 @@ const handleTransaction: HandleTransaction = async (
   // limiting this agent to emit only 5 findings so that the alert feed is not spammed
   if (findingsCount >= 5) return findings;
   if(isContractCreation(txEvent)) {
-    console.log(txEvent.transaction);
     const evm = new EVM(txEvent.transaction.data);
     const opCodes = evm.getOpcodes();
     const codes = opCodes.map((code: any) => {
       return code["name"]
     });
-    console.log(codes);
-    // opCodes.forEach((code : any) => {
-    //    if(code["pushData"] != null && typeof code["pushData"] != undefined) {
-    //      console.log(code["name"]);
-    //      console.log(code["pushData"]);
-    //      console.log("***************************************************")
-   
-    //    }
-    // })
-    // console.log(evm.getOpcodes());
+    
+    const hashedCodes = sha256(codes.join(""));
+    hashesOfContractsWeHaveSeen[hashedCodes] ||= [];
+
+    const deployment : Deployment = {
+      deployingContract: txEvent.transaction.from,
+      timestamp: (new Date()).getTime()
+    }
+    hashesOfContractsWeHaveSeen[hashedCodes].push(deployment);
+    console.log(hashesOfContractsWeHaveSeen);
+
   }
   return findings;
   // filter the transaction logs for Tether transfer events
